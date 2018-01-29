30227785d2724b405a3323f8d0b272f16d2ce2e6
Alberto Tonet <atonet@sitbelluno.it>
2018-01-29 16:29:16 +0100

    set current campionato

--- common/models/campionato.js
+++ common/models/campionato.js
@@ -1,5 +1,35 @@
 'use strict';
 
 module.exports = function(Campionato) {
-
+    
+    Campionato.setCurrent = function(id,cb) {
+        //console.log(id);
+        //set all records to current = 0
+        Campionato.updateAll({current: 0}, function (err){
+            if (err) cb(err);
+        });
+        // find instance
+        
+        Campionato.findById( id, function (err, instance) {
+            if (err) cb(err);
+            //console.log(instance);
+            //set to current = 1
+            instance.updateAttributes({current: 1}, function (err, instance){
+                if (err) cb(err);
+                if (!err){
+                    cb(null, instance);
+                    //console.log(instance.current);
+                }
+            });
+        });
+    };
+    Campionato.remoteMethod (
+        'setCurrent', 
+        {
+          description: "set current = 1 to id campionato and 0 to others",
+          http: {path: '/:id/set-current', verb: 'put'},
+          accepts: {arg: 'id', type: 'string', http: { source: 'path' } },
+          returns: {root: true, type: 'object'}
+        }
+    );
 };

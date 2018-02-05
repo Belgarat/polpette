334d7cbaeddf472325d8b0a5675ce0bd2bc5a3b0
Alberto Tonet <atonet@sitbelluno.it>
2018-02-05 15:57:55 +0100

    debug delete punteggio from squadra

--- common/models/squadra.js
+++ common/models/squadra.js
@@ -6,10 +6,10 @@ module.exports = function(Squadra) {
     * Set last update date after every update
     */
     Squadra.observe("before delete" , function(ctx, next) {
-        Squadra.findById( ctx.where.id, function (err, instance) {
+        Squadra.findById( ctx.where.id, {include: ['punteggio']}, function (err, instance) {
+            console.log(instance);
             //delete punteggio
-            instance.punteggio.destroy(function(err){
-            });
+            instance.punteggio.destroy(function(err){});
         });
         // ...then delete squadra
         next();

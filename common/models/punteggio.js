3162f6cafb2a6353923d1818fe9f0c1d8af482bc
Alberto Tonet <atonet@sitbelluno.it>
2018-02-26 16:52:51 +0100

    punteggio delete by id squadra

--- common/models/punteggio.js
+++ common/models/punteggio.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = function(Punteggio) {
-
+    /**
+    * Set last update date after every update
+    */
+    Punteggio.deleteByIdSquadra = function(idSquadra, next) {
+        Punteggio.find( {where: {squadraId: idSquadra}}, function (err, instance) {
+            //console.log(instance);
+            //delete punteggio
+            instance.punteggio.destroy(function(err){
+                console.log(err);
+            });
+        });
+        // ...then delete squadra
+        next();
+    };
+    Punteggio.remoteMethod (
+        'deleteByIdSquadra', 
+        {
+          description: "delete punteggio from id squadra",
+          http: {path: '/:idSquadra/delete-punteggio-by-id-suadra', verb: 'delete'},
+          accepts: {arg: 'idSquadra', type: 'string', http: { source: 'path' } },
+          returns: {root: true, type: 'object'}
+        }
+    );
 };
